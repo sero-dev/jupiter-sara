a3a12dd98310c314a93d3fa80e59498a782b7fa6
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-05-01 16:14:44 -0400

    Adds values to route

--- routes/phase-four.js
+++ routes/phase-four.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var values = {
+  title: 'Jupiter Search',
+  phase: 'Currently using Phase Four: Google API',
+  search: '/v4/search',
+  disabled: 'true',
+  showImport: 'true',
+  scriptLocation: '/javascripts/import.js'
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Jupiter Search',
-    phase: 'Currently using Phase Four: Google API'
-  });
+router.get('/', function(req, res) {
+  res.render('index', values);
+});
+
+router.get('/search', function(req, res) {
+  res.render('search_nav', values);
 });
 
 module.exports = router;
