6be677506a4c1a454eb2ccfe3f53f1d3beaf6feb
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-05-08 01:16:54 -0400

    Adds request to pull data from Google Search api

--- routes/phase-four.js
+++ routes/phase-four.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const search = require('../search.js');
+const login = require('../credentials');
+const request = require('request');
 
 let values = {
   title: 'Jupiter Search',
   phase: 'Currently using Phase Four: Google API',
   search: '/v4/search',
   showImport: 'true',
+  results: {},
   scriptLocation: '/javascripts/import.js'
 };
 
@@ -15,9 +17,15 @@ router.get('/', function(req, res) {
   res.render('index', values);
 });
 
-router.get('/search', function(req, res) {
-  let data = search.getGoogleResults();
-  res.render('search_nav', values);
+router.get('/search/', function(req, res) {
+  const url = `https://www.googleapis.com/customsearch/v1/?key=${login.key}&cx=${login.engineID}&q=${req.query.q}`;
+  values.title = `${req.query.q} - Jupiter Search`
+
+  request.get(url, (error, response, body) => {
+    let json = JSON.parse(body);
+    values.results = json.items;
+    res.render('results', values);
+  })
 });
 
 module.exports = router;
