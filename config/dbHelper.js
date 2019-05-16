a36714a52f0ee566ab1f576a61a61642c19fedfc
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-05-16 03:00:56 -0400

    Adds remote server to config

--- config/dbHelper.js
+++ config/dbHelper.js
@@ -1,7 +1,18 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('localdb', 'root', 'Anthony13;', {
-  host: 'localhost',
+// module.exports = new Sequelize('localdb', 'root', 'Anthony13;', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   logging: false,
+//   define: {
+//     timestamps: false,
+//     freezeTableName: true
+//   }
+// });
+
+module.exports = new Sequelize('rose4441', 'rose4441', '15234441;', {
+  port: 3306,
+  host: '149.4.211.180',
   dialect: 'mysql',
   logging: false,
   define: {
